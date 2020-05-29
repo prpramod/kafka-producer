b119382c5ee4eea75440cf787dfd0233d46412d0
pramodpra <pramod.pradhan@ephesoft.com>
2020-05-29 10:38:31 +0530

    Created FixedRateProducer which will automatically send message to topic t-fixedrate every second . EnabledScheduling in the SpringbootApplication class

--- src/main/java/com/course/kafkaproducer/KafkaProducerApplication.java
+++ src/main/java/com/course/kafkaproducer/KafkaProducerApplication.java
@@ -5,21 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class KafkaProducerApplication implements CommandLineRunner {
 
     @Autowired
-    HelloKafkaProducer helloKafkaProducer;
+   // HelloKafkaProducer helloKafkaProducer;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(KafkaProducerApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        helloKafkaProducer.sendHello("Pramod:"+Math.random());
+     //   helloKafkaProducer.sendHello("Pramod:"+Math.random());
 
     }
 }
---
+++ src/main/java/com/course/kafkaproducer/producers/FixedRateProducer.java
@@ -0,0 +1,32 @@
+package com.course.kafkaproducer.producers;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FixedRateProducer {
+
+    private int counter=0;
+    private Logger log = LoggerFactory.getLogger(FixedRateProducer.class);
+
+
+    private KafkaTemplate<String,String> kafkaTemplate;
+    @Autowired
+    public FixedRateProducer(KafkaTemplate kafkaTemplate){
+        this.kafkaTemplate=kafkaTemplate;
+
+    }
+
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage(){
+        counter++;
+        log.info("Counter is:"+counter);
+        kafkaTemplate.send("t-fixedrate","Fixed Rate"+counter);
+    }
+}
